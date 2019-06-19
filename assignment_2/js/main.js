340b5b5264651249456187405e5ea3ad3e022c7a
ADLSmit <a.d.l.smit@student.rug.nl>
2019-06-19 14:47:42 +0200

    Assignment 2

--- assignment_2/js/main.js
+++ assignment_2/js/main.js
@@ -4,59 +4,103 @@ function validateForm(){
         $("#form-alert").text("Name must be filled out").show();
         return false;
     }
+    if (name.match(/^[0-9]+$/) && name !== "") {
+        $("#form-alert").text("Name can't be in numbers").show();
+        return false;
+    }
 
     let age = $("#formGroupInput2").val();
     if (age === "") {
         $("#form-alert").text("Age must be filled out").show();
         return false;
     }
+    if (age.match(/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/) && age !== "") {
+        $("#form-alert").text("Age must be in numbers").show();
+        return false;
+    }
 
-    let mail = $("#formGroupInput3").val();
+    let city = $("#formGroupInput3").val();
+    if (city === ""){
+        $("#form-alert").text("City must be filled in").show();
+        return false;
+    }
+    if (city.match(/^[0-9]+$/) && city !== "") {
+        $("#form-alert").text("City can't be in numbers").show();
+        return false;
+    }
+
+    let mail = $("#formGroupInput4").val();
     if (mail === "") {
-        $("#myForm").submit(function(e) {
-            e.preventDefault();
-        });
         $("#form-alert").text("Mail must be filled out").show();
         return false;
     }
-
-    let city = $("#formGroupInput4").val();
-    if (city === "") {
-        $("#form-alert").text("City must be filled out").show();
+    if (mail.match(/^[0-9]+$/) && mail !== "") {
+        $("#form-alert").text("Mail can't be in numbers").show();
         return false;
     }
+
 
     let num = $("#formGroupInput5").val();
     if (num === "") {
-        alert("Num must be filled out");
         $("#form-alert").text("Phone number must be filled out").show();
         return false;
     }
+    if (num.match(/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/) && city !== "") {
+        $("#form-alert").text("Phone number must be in numbers").show();
+        return false;
+    }
 
+    return true;
 }
 
 function writeFormData(){
-    if(validateForm()){
-        console.log("valid form")
-
-    }
 
 }
 
-function createTabs(){
+function formErase() {
+    $("#form-content").hide();
+    $("#form-alert").hide();
+}
 
+function linktabs() {
+    // make the tabs work
+    $("#link-tab").on('click', function () {
+        $("#link-tab").addClass("active");
+        $("#contact-tab").removeClass("active");
+        $("#links").addClass("active");
+        $("#contact").removeClass("active");
+
+    });
+}
+
+
+function contacttabs(){
+    $("#contact-tab").on('click', function(){
+        $("#link-tab").removeClass("active");
+        $("#contact-tab").addClass("active");
+        $("#links").removeClass("active");
+        $("#contact").addClass("active");
+    });
+}
+
+function showcontent(){
+    $("#form-content").show();
 }
 
 $(function() {
-    writeFormData();
+    linktabs();
+    contacttabs();
 
-    $('ul.nav li').click(function(){
-        var tab_id = $(this).attr('data-tab');
+    $("#submit").on('click', function(){
+        if (validateForm()) {
+            showcontent();
+            $("#form-alert").hide();
+        }
+    });
 
-        $('ul.nav li').removeClass('current');
-        $('.tab-content').removeClass('current');
+    $("#erase").on('click', function(){
+        formErase();
+    });
 
-        $(this).addClass('current');
-        $("#"+tab_id).addClass('current');
-    })
 });
+
